3daee2fcbcfa8c9c2e7a2e45e37095728fb63463
Santi2007939 <117610911+Santi2007939@users.noreply.github.com>
2023-02-13 20:07:51 -0500

    HTML y CSS finalizados

---
+++ progra.js
@@ -0,0 +1,14 @@
+/*Convertir-Botones*/ 
+const btnBi = document.getElementById("btn-Bi");
+const btnCe = document.getElementById("btn-Ce");
+const btnRe = document.getElementById("btn-Re");
+
+/*Intercambiar-Convertir*/
+const ccIntercambiar = document.getElementById("cc-intercambiar");
+
+const cesarConvertirCaT = document.getElementById("ce-convertir-texto-a-codigo");
+const binarioConvertirCaT = document.getElementById("cc-convertir-texto-a-codigo");
+const reversaConvertir = document.getElementById("revertir-texto");
+
+/*Separado-Junto*/
+const sepYJuntar = document.getElementById("sep-jun");
